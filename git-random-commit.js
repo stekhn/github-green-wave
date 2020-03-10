6c8219d6b65d41aff81f44c07c9bb02e51efcc10
Steffen Kühne <stekhn@gmail.com>
2020-03-10 22:55:12 +0100

    Add test for virtual file system

--- git-random-commit.js
+++ git-random-commit.js
@@ -1,8 +1,16 @@
 const fs = require('memfs');
 const git = require('isomorphic-git');
 const http = require('isomorphic-git/http/node')
+
+const url = 'https://github.com/isomorphic-git/lightning-fs.git'
 const dir = '/';
+  
+async function clone() {
+  return await git.clone({ fs, http, dir, url })
+}
 
-fs.writeFileSync('/hello.txt', 'World!');
-
-git.clone({ fs, http, dir, url: 'https://github.com/isomorphic-git/lightning-fs.git' }).then(console.log)
+(async function init() {
+  const log = await clone();
+  const ls = fs.readdirSync(dir);
+  console.log(ls);
+})()
