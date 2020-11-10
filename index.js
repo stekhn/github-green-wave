65b3d206038058c43fecd88beb5d22f5edbf0d74
Steffen Kühne <stekhn@gmail.com>
2020-11-10 21:08:58 +0100

    Add random chance for a commit to happen

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const git = require('isomorphic-git');
 const http = require('isomorphic-git/http/node');
 
 const config = require('./config.json');
-const { dir, filename } = config;
+const { dir, filename, probability } = config;
 const filepath = `${dir}${filename}`;
 const gitpath = `${dir}.git`;
 const content = new Date().toISOString();
@@ -12,12 +12,14 @@ url.username = config.auth.username;
 url.password = config.auth.token || config.auth.password;
 
 exports.githubGreenWave = async () => {
-  await gitClone().catch(handleError);
-  await writeFile().catch(handleError);
-  await gitAdd().catch(handleError);
-  await gitCommit().catch(handleError);
-  await gitPush().catch(handleError);
-  await removeGit().catch(handleError);
+  if (Math.random() >= probability) {
+    await gitClone().catch(handleError);
+    await writeFile().catch(handleError);
+    await gitAdd().catch(handleError);
+    await gitCommit().catch(handleError);
+    await gitPush().catch(handleError);
+    await removeGit().catch(handleError);
+  } 
 };
 
 function handleError(err) {
